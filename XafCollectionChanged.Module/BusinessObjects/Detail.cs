08e322f36678aed3f257d58e365233a1a46e1bbd
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-09-20 20:06:42 +0300

    Add project files.

---
+++ XafCollectionChanged.Module/BusinessObjects/Detail.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafCollectionChanged.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Detail : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Detail(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        Master master;
+        bool active;
+
+        public bool Active
+        {
+            get => active;
+            set => SetPropertyValue(nameof(Active), ref active, value);
+        }
+        
+        [Association("Master-Details")]
+        public Master Master
+        {
+            get => master;
+            set => SetPropertyValue(nameof(Master), ref master, value);
+        }
+    }
+}
---
+++ XafCollectionChanged.Module/Controllers/CollectionChangedController.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace XafCollectionChanged.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class CollectionChangedController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public CollectionChangedController()
+        {
+            InitializeComponent();
+            this.TargetViewType = ViewType.DetailView;
+            this.TargetViewNesting = Nesting.Root;
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        void Collection_CollectionChanged(object sender, XPCollectionChangedEventArgs e)
+        {
+            Debug.WriteLine("CollectionChanged:"+e.CollectionChangedType.ToString());
+        }
+        void View_CurrentObjectChanged(object sender, EventArgs e)
+        {
+            MapEvents();
+        }
+
+        private void MapEvents()
+        {
+            if (this.View.CurrentObject != null)
+            {
+                var Instance = this.View.CurrentObject as BaseObject;
+                Instance.ClassInfo.Members.Where(m => m.IsCollection).ToList().ForEach(m =>
+                {
+                    var collection = Instance.GetMemberValue(m.Name) as XPBaseCollection;
+                    collection.CollectionChanged += Collection_CollectionChanged;
+                });
+            }
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            this.View.CurrentObjectChanged += View_CurrentObjectChanged;
+            MapEvents();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
